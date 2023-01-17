3be2aed0c5bdd6f3964e6201e4ff710c26c4ea44
Romulo Banares <romulo.banares.contractor@bbva.com>
2023-01-17 22:10:30 +0100

    create project groups

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkView.swift
+//  Landmarks
+//
+//  Created by ROMULO BAÑARES ROCA on 17/1/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
